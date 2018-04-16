8add4875ab1c14fb5b7965a982bb724902cc548d
Patricia Nykaza <pnykaza@calstatela.edu>
2018-04-16 19:32:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |value, index|
+    puts "#{index}: #{value}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  name = array.map! {|name| name.capitalize } 
+  name.each { |one| puts "#{one}!" } 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |name|
+      if name.length > 4
+        return true 
+      elsif name.length < 4 || name.length = 4 
+        return false 
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
